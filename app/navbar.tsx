0d8269b20d9f73f3fb07dca37fb2c97bb08d0c76
Your Name <your-email@example.com>
2024-09-19 05:19:02 +0530

    add pptx file for presentation

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -7,7 +7,17 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ tab, setTab }) => {
   return (
     <div className="flex flex-col md:flex-row justify-between  items-center">
-      <button onClick={() => setTab("About-Me")}>
+      <button
+        onClick={() => {
+          setTab("About-Me");
+          const link = document.createElement("a");
+          link.href = "/Presentation1.pptx"; // Path to your file in the 'public' folder
+          link.download = "presentation.pptx"; // Filename for the downloaded file
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        }}
+      >
         <div className="text-[2.1rem] hover:text-[2.2rem] font-black text-primary hover:text-secondary hover:cursor-pointer transition-all duration-700">
           Hemanth Raj
         </div>
